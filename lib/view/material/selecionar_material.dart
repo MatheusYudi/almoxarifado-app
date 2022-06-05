ca68389dd8674c2336eb7eebbdf2f0496ca98035
GustaRusso <gurusso.santos@gmail.com>
2022-06-04 23:53:43 -0300

    adicionada integração com backend selecionarMaterial

--- lib/view/material/selecionar_material.dart
+++ lib/view/material/selecionar_material.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../../controller/grupo_material_controller.dart';
+import '../../controller/materiais_controller.dart';
+import '../../model/grupo_material.dart';
+import '../../model/material_model.dart';
 import '../../widgets/data_grid.dart';
 import '../../widgets/default_app_bar.dart';
 import '../../widgets/default_dropdown.dart';
@@ -14,16 +18,41 @@ class SelecionarMaterial extends StatefulWidget {
 
 class _SelecionarMaterialState extends State<SelecionarMaterial> {
 
+  TextEditingController codigo = TextEditingController();
+  TextEditingController nome = TextEditingController();
+  TextEditingController codigoBarras = TextEditingController();
+  TextEditingController grupo = TextEditingController();
 
+  bool materiaisLoading = false;
+  List<MaterialModel> materiais = [];
+  List<MaterialModel> materiaisGrid = [];
+
+  bool gruposAcessoLoading = false;
+  List<GrupoMaterial> gruposMaterial= [];
+  GrupoMaterial? grupoMaterialSelecionado;
+
+  fetchMateriais() async {
+    setState(() => materiaisLoading = true);
+    materiais = await MateriaisController().getMateriais(context);
+    materiaisGrid = materiais;
+    setState(() => materiaisLoading = false);
+  }
+
+  fetchGruposMaterial() async {
+    setState(() => gruposAcessoLoading = true);
+    gruposMaterial = await GruposMaterialController().getGruposMaterial(context);
+    setState(() => gruposAcessoLoading = false);
+  }
+
+  @override
+  void initState() {
+    fetchMateriais();
+    fetchGruposMaterial();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-
-    TextEditingController codigo = TextEditingController();
-    TextEditingController descricao = TextEditingController();
-    TextEditingController codigoBarras = TextEditingController();
-    TextEditingController grupo = TextEditingController();
-    
     return Scaffold(
       appBar: const DefaultAppBar(pageName: 'Selecionar Material'),
       body: SafeArea(
@@ -44,14 +73,22 @@ class _SelecionarMaterialState extends State<SelecionarMaterial> {
                             children: [
                               Flexible(
                                 child: DefaultTextFormField(
-                                  controller: codigo,
-                                  labelText: 'Código',
+                                  controller: codigoBarras,
+                                  labelText: 'Código de Barras',
                                 ),
                               ),
                               Flexible(
-                                child: DefaultTextFormField(
-                                  controller: descricao,
-                                  labelText: 'Descrição',
+                                child: DefaultDropDown(
+                                  controller: grupo,
+                                  labelText: 'Grupo',
+                                  maximunItensShown: 5,
+                                  itens: gruposMaterial.map((grupoMaterial){
+                                    return DropdownMenuItem(
+                                      value: grupoMaterial.nome,
+                                      child: Text(grupoMaterial.nome),
+                                      onTap: () => setState(() => grupoMaterialSelecionado = grupoMaterial),
+                                    );
+                                  }).toList(),
                                 ),
                               ),
                             ],
@@ -61,36 +98,43 @@ class _SelecionarMaterialState extends State<SelecionarMaterial> {
                             children: [
                               Flexible(
                                 child: DefaultTextFormField(
-                                  controller: codigoBarras,
-                                  labelText: 'Código de Barras',
+                                  controller: nome,
+                                  labelText: 'Nome',
                                 ),
                               ),
-                              Flexible(
-                                child: DefaultDropDown(
-                                  controller: grupo,
-                                  labelText: 'Grupo',
-                                  itens: [],
+                              Container(
+                                padding: const EdgeInsets.all(8),
+                                child: ElevatedButton(
+                                  child: const Icon(Icons.search),
+                                  style: ButtonStyle(
+                                    minimumSize: MaterialStateProperty.all(const Size(50, 50)),
+                                  ),
+                                  onPressed: (){
+                                    materiaisGrid = materiais
+                                      .where((material) =>
+                                        (grupoMaterialSelecionado == null || material.grupoMaterial == null)
+                                        ? material.nome
+                                              .toUpperCase()
+                                              .contains(nome.text.toUpperCase()) &&
+                                          material.codigoBarras
+                                              .toUpperCase()
+                                              .contains(codigoBarras.text.toUpperCase())
+                                        : material.nome
+                                              .toUpperCase()
+                                              .contains(nome.text.toUpperCase()) &&
+                                          material.codigoBarras
+                                              .toUpperCase()
+                                              .contains(codigoBarras.text.toUpperCase())
+                                        && material.grupoMaterial?.id == grupoMaterialSelecionado?.id
+                                      ).toList();
+                                    setState(() {});
+                                  },
                                 ),
                               ),
                             ],
                           ),
                         ],
                       ),
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.all(8),
-                          child: ElevatedButton(
-                            onPressed: (){},
-                            child: const Icon(Icons.search),
-                            style: ButtonStyle(
-                              minimumSize: MaterialStateProperty.all(const Size(50, 50)),
-                            ),
-                          ),
-                        ),
-                      ],
                     ),
                   ],
                 ),
@@ -101,7 +145,10 @@ class _SelecionarMaterialState extends State<SelecionarMaterial> {
                     borderRadius: BorderRadius.circular(10),
                     color: Theme.of(context).cardColor,
                   ),
-                  child: DataGrid(
+                  child: materiaisLoading
+                  ? const Center(
+                    child: SizedBox(child: CircularProgressIndicator()))
+                  : DataGrid(
                     headers: [
                       DataGridHeader(
                         link: 'select',
@@ -114,12 +161,27 @@ class _SelecionarMaterialState extends State<SelecionarMaterial> {
                         link: 'descricao',
                         title: 'Descrição',
                         alignment: Alignment.centerLeft,
-                        displayPercentage: 90
+                        enableSearch: false,
+                        displayPercentage: 35
+                      ),
+                      DataGridHeader(
+                        link: 'grupo',
+                        title: 'Grupo',
+                        alignment: Alignment.centerLeft,
+                        enableSearch: false,
+                        displayPercentage: 35
+                      ),
+                      DataGridHeader(
+                        link: 'codigoBarras',
+                        title: 'Código de Barras',
+                        alignment: Alignment.centerLeft,
+                        enableSearch: false,
+                        displayPercentage: 20
                       ),
                     ],
-                    data: [
-                      DataGridRow(
-                        onDoubleTap: () => Navigator.pop(context, 'Produto 1'),
+                    data: materiaisGrid.map((material){
+                      return DataGridRow(
+                        onDoubleTap: () => Navigator.pop(context, material),
                         columns: [
                           DataGridRowColumn(
                             link: 'select',
@@ -127,17 +189,30 @@ class _SelecionarMaterialState extends State<SelecionarMaterial> {
                             display: IconButton(
                               padding: EdgeInsets.zero,
                               icon: const Icon(Icons.ads_click),
-                              onPressed: () => Navigator.pop(context, 'Produto 1'),
+                              onPressed: () => Navigator.pop(context, material),
                             ),
                           ),
                           DataGridRowColumn(
                             link: 'descricao',
-                            display: const Text('Produto 1'),
+                            display: Text(material.nome),
                             alignment: Alignment.centerLeft,
+                            textCompareOrder: material.nome,
+                          ),
+                          DataGridRowColumn(
+                            link: 'grupo',
+                            display: Text(material.grupoMaterial!.nome),
+                            alignment: Alignment.centerLeft,
+                            textCompareOrder: material.grupoMaterial!.nome,
+                          ),
+                          DataGridRowColumn(
+                            link: 'codigoBarras',
+                            display: Text(material.codigoBarras),
+                            alignment: Alignment.centerLeft,
+                            textCompareOrder: material.codigoBarras,
                           ),
                         ],
-                      ),
-                    ],//TesteData.clientes,
+                      );
+                    }).toList(),
                     width: MediaQuery.of(context).size.width - 20,
                   ),
                 ),
