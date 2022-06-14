5831dfa3b8c22233a10c721d52c6b36ae9707c59
MatheusYudi <matheusyudi2909@gmail.com>
2022-06-13 21:35:09 -0300

    fix

--- lib/controller/materiais_controller.dart
+++ lib/controller/materiais_controller.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -7,115 +6,115 @@ import '../api/api_response.dart';
 import '../model/material_model.dart';
 import 'funcionario_atual_controller.dart';
 
-class MateriaisController
-{
-
+class MateriaisController {
   String error = '';
 
-  Future<List<MaterialModel>> getMateriais(BuildContext context, [Map? material]) async
-  {
+  Future<List<MaterialModel>> getMateriais(BuildContext context,
+      [Map? material]) async {
     ApiResponse response = await ApiClient().get(
       endPoint: 'material?page=1&size=1000&order=id&orderBy=DESC&status=Ativo',
-      token: Provider.of<FuncionarioAtualController>(context, listen: false).getFuncionarioAtual().tokenApi,
+      token: Provider.of<FuncionarioAtualController>(context, listen: false)
+          .getFuncionarioAtual()
+          .tokenApi,
     );
 
-    if(response.statusCode != 200)
-    {
+    if (response.statusCode != 200) {
       throw Exception(response.body['error']);
     }
-    return response.body['data']['rows'].map<MaterialModel>((material) => MaterialModel.fromJson(material)).toList();
+    return response.body['data']['rows']
+        .map<MaterialModel>((material) => MaterialModel.fromJson(material))
+        .toList();
   }
 
-  Future<MaterialModel> getMaterialById(BuildContext context, int id) async
-  {
+  Future<MaterialModel> getMaterialById(BuildContext context, int id) async {
     ApiResponse response = await ApiClient().get(
       endPoint: 'material/$id',
-      token: Provider.of<FuncionarioAtualController>(context, listen: false).getFuncionarioAtual().tokenApi,
+      token: Provider.of<FuncionarioAtualController>(context, listen: false)
+          .getFuncionarioAtual()
+          .tokenApi,
     );
-    
-    if(response.statusCode != 200)
-    {
+
+    if (response.statusCode != 200) {
       throw Exception(response.body['error']);
     }
     return MaterialModel.fromJson(response.body['data']);
   }
 
-  Future<bool> deleteMaterial(BuildContext context, int id) async
-  {
+  Future<bool> deleteMaterial(BuildContext context, int id) async {
     ApiResponse response = await ApiClient().delete(
       endPoint: 'material/$id',
-      token: Provider.of<FuncionarioAtualController>(context, listen: false).getFuncionarioAtual().tokenApi,
+      token: Provider.of<FuncionarioAtualController>(context, listen: false)
+          .getFuncionarioAtual()
+          .tokenApi,
     );
-    
-    if(response.statusCode != 200)
-    {
+
+    if (response.statusCode != 200) {
       throw Exception(response.body['error']);
     }
     return true;
   }
 
-  Future<MaterialModel?> updateMaterial(BuildContext context, MaterialModel material) async
-  {
+  Future<MaterialModel?> updateMaterial(
+      BuildContext context, MaterialModel material) async {
     ApiResponse response = await ApiClient().put(
       endPoint: 'material',
-      token: Provider.of<FuncionarioAtualController>(context, listen: false).getFuncionarioAtual().tokenApi,
+      token: Provider.of<FuncionarioAtualController>(context, listen: false)
+          .getFuncionarioAtual()
+          .tokenApi,
       data: material.toJson(),
     );
-    
-    if(response.statusCode > 299)
-    {
-      response.body['error'].forEach((requestError){
+
+    if (response.statusCode > 299) {
+      response.body['error'].forEach((requestError) {
         error += requestError['msg'] + "\n";
       });
-    }
-    else
-    {
+    } else {
       return material;
     }
     return null;
   }
 
-  Future<MaterialModel?> postMaterial(BuildContext context, MaterialModel material) async
-  {
+  Future<MaterialModel?> postMaterial(
+      BuildContext context, MaterialModel material) async {
     ApiResponse response = await ApiClient().post(
       endPoint: 'material',
-      token: Provider.of<FuncionarioAtualController>(context, listen: false).getFuncionarioAtual().tokenApi,
+      token: Provider.of<FuncionarioAtualController>(context, listen: false)
+          .getFuncionarioAtual()
+          .tokenApi,
       data: material.toJson(),
     );
-    
-    if(response.statusCode > 299)
-    {
-      response.body['error'].forEach((requestError){
+
+    if (response.statusCode > 299) {
+      response.body['error'].forEach((requestError) {
         error += requestError['msg'] + "\n";
       });
-    }
-    else
-    {
+    } else {
       return material;
     }
     return null;
   }
 
 //TODO confirmar qual email vai ser enviada a solicitação
-  Future<bool> solicitarCompra(BuildContext context) async
-  {
+  Future<bool> solicitarCompra(BuildContext context) async {
     ApiResponse response = await ApiClient().post(
       endPoint: 'material/purchase-request',
-      token: Provider.of<FuncionarioAtualController>(context, listen: false).getFuncionarioAtual().tokenApi,
-      data: Provider.of<FuncionarioAtualController>(context, listen: false).getFuncionarioAtual().email,
+      token: Provider.of<FuncionarioAtualController>(context, listen: false)
+          .getFuncionarioAtual()
+          .tokenApi,
+      data: {
+        "email": Provider.of<FuncionarioAtualController>(context, listen: false)
+            .getFuncionarioAtual()
+            .email,
+      },
     );
-    
-    if(response.statusCode > 299)
-    {
-      response.body['error'].forEach((requestError){
+
+    if (response.statusCode > 299) {
+      response.body['error'].forEach((requestError) {
         error += requestError['msg'] + "\n";
       });
-    }
-    else
-    {
+    } else {
       return true;
     }
     return false;
   }
-
 }
